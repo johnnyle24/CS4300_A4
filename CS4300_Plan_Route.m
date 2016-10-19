a900d59ed8cb399959890fbfdb45750f82b766c6
trungl <trungl@lab1-12.eng.utah.edu>
2016-10-19 15:31:15 -0600

    updated plan route

--- CS4300_Plan_Route.m
+++ CS4300_Plan_Route.m
@@ -13,6 +13,21 @@ function plan = CS4300_Plan_Route(current,target,safe)
 %3 Wumpus
 %4 Gold
 
+initial_state = zeros(1,3);
+initial_state(1) = current(1);
+initial_state(2) = current(2);
+initial_state(3) = 0;
+
+goal_state = zeros(1,3);
+goal_state(1) = target(1);
+goal_state(2) = target(2);
+goal_state(3) = 0;
+
+[sol,nod] = CS4300_Wumpus_A_star_safe(actual_board,initial_state,goal_state,'CS4300_A_star_Man',safe);
+
+for i = 1:length(sol)
+   plan(i) = sol(i,4); 
+end
 
 
 end
